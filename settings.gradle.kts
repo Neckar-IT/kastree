095dd0e86c31ce0e8bd96896b0e1f30d9ea1557f
Johannes Schneider <js@neckar.it>
2023-09-13 10:15:26 +0200

    refresh versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,8 +13,19 @@ plugins {
   //Prints the Maven like output at the end of the build
   //https://kordamp.org/kordamp-gradle-plugins/#_org_kordamp_gradle_insight
   id("org.kordamp.gradle.insight") version "0.47.0"
+////                           # available:"0.48.0"
+////                           # available:"0.49.0"
+////                           # available:"0.50.0"
+////                           # available:"0.51.0"
 
   id("de.fayard.refreshVersions") version "0.40.2"
+////                          # available:"0.50.0"
+////                          # available:"0.50.1"
+////                          # available:"0.50.2"
+////                          # available:"0.51.0"
+////                          # available:"0.60.0"
+////                          # available:"0.60.1"
+////                          # available:"0.60.2"
 }
 
 configure<org.kordamp.gradle.plugin.insight.InsightExtension> {
